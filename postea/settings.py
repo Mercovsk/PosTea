1ed32c66e13f3f4254517b707ec9b0c6034af50d
Mercovsk <marcbaja08@gmail.com>
2023-03-29 16:46:16 +0800

    updated settings.py

--- postea/settings.py
+++ postea/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-41!e+6^l(mi=7lv!c3b1lnx7kjpztdft9gu&x3!su6+yeubs7f'
+SECRET_KEY = secret.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
